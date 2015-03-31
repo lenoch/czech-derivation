6c1b8f5fd76e2b7c6ff5d613fe20204a6b522418
NeugeCZ <399209@mail.muni.cz>
2015-03-31 10:55:55 +0200

    Update deadjektiva.py
    Tak ten seznam dvojic celý kód podstatně zkrátil, nakonec jsem užil rozeznávání dvoji/trojic posledních znaků (kvůli občasnému měkčení "e" a případům ský/cký/chý, které se chovají trochu jinak). Tak snad to bude použitelný.

--- deadjektiva.py
+++ deadjektiva.py
@@ -31,3 +31,16 @@ def mladost(lemma, atributy, vyznamy):
         del atributy['d']
         atributy.update(k='1', g='F')  # genus, jmenný rod
         yield lemma[:-1] + 'ost', atributy, vyznamy
+        
+def adverbializace(lemma, atributy, vyznamy):
+    zmeny=[('dý','dě'),('rý','ře'),('bý','bě'),('vý','vě'),('mý','mě'),('sý','se'),('pý','pě'),('tý','tě'),('lý','le'),('ný','ně'),('ní','ně'),('chý','še'),('hý','ze'),('ský','sky'),('cký','cky'),('ký','ce')]    
+    if atributy.get('k') == '2' and atributy.get('d') ==  '1':
+        if lemma.endswith('á') or lemma.endswith('é'): lemma = lemma[:-1] + 'ý'   #Prozatím primitivní koverze na maskulina, nepočítá s krátkými tvary (mlád, zdráva...)
+        atributy['k'] = '6'     
+        yield lemma[:-1] + 'o', atributy, vyznamy
+        for pravidla in zmeny:   #Kvůli ský/cký/chý používám dva cykly. první na rozeznávání posledních tří znaků, druhý na poslední dva znaky
+            if lemma[len(lemma)-3:] in pravidla:
+                yield lemma[:len(lemma)-3] + pravidla[1], atributy, vyznamy
+                break   #Breakuju kvůli pravidlům na "ký" a "hý", která se znovu aplikovala i pro na už jednou zpracovaná "cký/ský" popř. "chý"
+            else:
+                if lemma[len(lemma)-2:] in pravidla: yield lemma[:len(lemma)-2] + pravidla[1], atributy, vyznamy    
