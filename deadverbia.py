9cbbf6b84cb1827c5a1a11eef6d26b406fe98acb
NeugeCZ <399209@mail.muni.cz>
2015-03-31 11:17:57 +0200

    Create deadverbia.py
    Funkční kostra., Není tam výše zmíněné přebírání seznamu dvojic, logování, oznamování vstupu chybného SD (nevím, jestli se má ošetřovat až tady nebo už v řídícím scriptu při volání) atd...

---
+++ deadverbia.py
@@ -0,0 +1,54 @@
+#Jsem lama a nejde mi importovat promenná "zmeny" z deadjektiv.
+
+def adv_derivace(lemma, atributy, vyznamy,zmeny): #Odvozování adverbií na základě prefixace a sufixace, na další jsem zatím nenarazil
+    if atributy.get('k') == '6':
+        if lemma.endswith('o'): yield 'na' + lemma, atributy, vyznamy
+        for pravidla in zmeny: 
+            if lemma[len(lemma)-3:] in pravidla: #Nejdřív používám rozeznávání posledních třech znaků kvůli případu chý/hý
+                lemma = lemma[:len(lemma)-3] + pravidla[0]
+                yield 'do' + lemma[:-1] + 'a', atributy, vyznamy
+                if lemma[0] == 'z' or lemma[0] == 's': yield 'ze' + lemma[:-1] + 'a', atributy, vyznamy
+                else: yield 'z' + lemma[:-1] + 'a', atributy, vyznamy
+                yield 'na' + lemma[:-1] + 'o', atributy, vyznamy
+                break 
+                    
+            else:
+                if lemma[len(lemma)-2:] in pravidla:
+                    lemma = lemma[:len(lemma)-2] + pravidla[0]
+                    yield 'do' + lemma[:-1] + 'a', atributy, vyznamy
+                    if lemma[0] == 'z' or lemma[0] == 's': yield 'ze' + lemma[:-1] + 'a', atributy, vyznamy
+                    else: yield 'z' + lemma[:-1] + 'a', atributy, vyznamy
+                    yield 'na' + lemma[:-1] + 'o', atributy, vyznamy
+                    break
+            
+def adv_stupnovani(lemma, atributy, vyznamy):
+    if atributy.get('k') == '6':
+        if atributy.get('d') == '1':
+            komparativ=[]                   #Kvůli množství (snad všech) výjimek níže, které mají více variant, jsem udělal komparativ jako seznam
+            atributy['d'] = '2'
+            if lemma=='dobře': komparativ = ['lépe', 'líp']   
+            elif lemma=='zle' or lemma=='špatně': komparativ = ['hůře', 'hůř']
+            elif lemma=='brzy': komparativ = ['dříve', 'dřív']
+            elif lemma=='dlouze' or lemma=='dlouho': komparativ = ['déle']
+            elif lemma=='vysoce' or lemma=='vysoko': komparativ = ['výš', 'výše']
+            elif lemma=='málo': komparativ = ['méně', 'míň']
+            elif lemma=='těžko' or lemma=='těžce': komparativ = ['tíže', 'tíž']
+            elif lemma=='snadno' or lemma=='snadně': komparativ = ['snáze', 'snáz', 'snadněji']
+            elif lemma=='hluboko' or lemma=='hluboce': komparativ = ['hloub', 'blouběji']
+            elif lemma=='široko' or lemma=='široce': komparativ = ['šíře', 'šíř', 'šířeji']
+            elif lemma=='úzko' or lemma=='úzce': komparativ = ['úže', 'úžeji']
+                
+            else:
+                if lemma.endswith('ce'): komparativ.append(lemma[:-2] + 'čeji')
+                elif lemma.endswith('cky'): komparativ.append(lemma[:-3] + 'čtěji')
+                elif lemma.endswith('sky'): komparativ.append(lemma[:-3] + 'štěji')
+                elif lemma.endswith('e') or lemma.endswith('ě'): komparativ.append(lemma + 'ji')
+
+            for slovo in komparativ:
+                yield slovo, atributy, vyznamy
+
+        if atributy.get('d') == '2':
+            atributy['d'] = '3'
+            if len(komparativ) == 0: komparativ.append(lemma)           #Pokud nedošlo ke koverzi z 1. stupně, tak se za komparativ vezme vstupní slovo
+            for slovo in komparativ:
+                yield 'nej' + slovo, atributy, vyznamy
