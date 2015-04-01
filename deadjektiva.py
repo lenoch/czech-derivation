8939c5eecce907d522c4aa58611704b6bf3862cb
Ondřej Svoboda <ondrej@svobodasoft.cz>
2015-04-01 13:49:02 +0200

    Možná lepší jména pro proměnné, coding style a poznámky

--- deadjektiva.py
+++ deadjektiva.py
@@ -8,6 +8,7 @@ def deadjektiva(lemma, atributy, vyznamy):
     return chain(
         stupnovani(lemma, dict(atributy), set(vyznamy)),
         mladost(lemma, dict(atributy), set(vyznamy)),
+        adverbializace(lemma, dict(atributy), set(vyznamy)),
     )
 
 
@@ -26,33 +27,47 @@ def stupnovani(lemma, atributy, vyznamy):
         atributy['d'] = '3'
         yield ('nej' + lemma, atributy, vyznamy)
 
+
 def mladost(lemma, atributy, vyznamy):
     if atributy.get('d') == '1' and lemma.endswith('ý'):
         del atributy['d']
         atributy.update(k='1', g='F')  # genus, jmenný rod
         yield lemma[:-1] + 'ost', atributy, vyznamy
-        
+
+
 def adverbializace(lemma, atributy, vyznamy):
-    zmeny=[('dý','dě'),('rý','ře'),('bý','bě'),('vý','vě'),('mý','mě'),\
-    ('sý','se'),('pý','pě'),('tý','tě'),('lý','le'),('ný','ně'),('ní','ně'),\
-    ('chý','še'),('hý','ze'),('ský','sky'),('cký','cky'),('ký','ce')]  
-    
-    if atributy.get('d') ==  '1':
-        atributy['k'] = '6'     
+    zakonceni = [  # dají se zkrátit díky „pravopisným“ úpravám
+        ('dý', 'dě'),
+        ('rý', 'ře'),  # rě → ře (už máme)
+        ('bý', 'bě'),
+        ('vý', 'vě'),
+        ('mý', 'mě'),
+        ('sý', 'se'),  # sě → se (čeština už nemá měkké ś)
+        ('pý', 'pě'),
+        ('tý', 'tě'),
+        ('lý', 'le'),  # lě → le (ani měkké ľ)
+        ('ný', 'ně'),
+        ('ní', 'ně'),
+        ('chý', 'še'),  # chě → še (historická palatalizace)
+        ('hý', 'ze'),  # hě → ze (podobně)
+        ('ský', 'sky'),
+        ('cký', 'cky'),
+        ('ký', 'ce'),  # kě → ce (a taky)
+    ]
+
+    if atributy.get('d') == '1':
+        atributy['k'] = '6'
         yield lemma[:-1] + 'o', atributy, vyznamy
-        
-        #Kvůli ský/cký/chý používám dva cykly. 
-        #První na rozeznávání posledních tří znaků, druhý na poslední dva znaky
-        #Do pravidla se v kazdem cyklu ulozi nektera z dvojic ve zmenach
-        
-        for pravidla in zmeny:
-            if lemma[len(lemma)-3:] in pravidla:
-                yield lemma[:-3] + pravidla[1], atributy, vyznamy
-                break  
-            
-                #Breakuju kvůli pravidlům na "ký" a "hý", která se znovu aplikovala 
-                #i pro na už jednou zpracovaná "cký/ský" popř. "chý"
-            
-            else:
-                if lemma[len(lemma)-2:] in pravidla: 
-                    yield lemma[:-2] + pravidla[1], atributy, vyznamy    
+
+        # Kvůli ský/cký/chý používám dva cykly.
+        # První na rozeznávání posledních tří znaků, druhý na poslední dva znaky
+        # Do pravidla se v kazdem cyklu ulozi nektera z dvojic ve zmenach
+
+        for adjektivni, adverbialni in zakonceni:
+            if lemma.endswith(adjektivni):
+                delka_zakonceni = len(adjektivni)
+                yield lemma[:-delka_zakonceni] + adverbialni, atributy, vyznamy
+
+                # Breakuju kvůli pravidlům na "ký" a "hý", která se znovu aplikovala
+                # i pro na už jednou zpracovaná "cký/ský" popř. "chý"
+                break
